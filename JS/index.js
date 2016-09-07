418d7da23710b8f56003906970b10530068b31b9
hjkws <755201617@qq.com>
2016-09-07 16:44:22 +0800

    hjkws

--- JS/index.js
+++ JS/index.js
@@ -86,6 +86,12 @@ window.onload=function(){
 	 aLi3[0].onclick=function(){
 	  window.open('four/product_7.html','_blank')	 
 	}
+	 aLi3[1].onclick=function(){
+	  window.open('four/product_9.html','_blank')	 
+	}
+	 aLi3[2].onclick=function(){
+	  window.open('four/product_8.html','_blank')	 
+	}
 	
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////////
